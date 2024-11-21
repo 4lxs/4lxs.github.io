82f3815b47d14e9121cfe6cf1bc29eebc039105a
svl <66408983+4lxs@users.noreply.github.com>
2024-11-21 15:19:39 +0100

    move to mkdocs

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages.default = pkgs.hello;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.alejandra pkgs.python312Packages.mkdocs-material pkgs.python312Packages.mkdocs-awesome-pages-plugin ];
+        };
+      };
+      flake = {
+      };
+    };
+}
